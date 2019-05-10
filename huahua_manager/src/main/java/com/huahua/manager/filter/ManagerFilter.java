25aedecc01982ba6bd6b6fd5ad01ccff46c8eb68
huahua.git <395616344@qq.com>
2019-05-10 11:04:09 +0800

    ff52

--- huahua_manager/src/main/java/com/huahua/manager/filter/ManagerFilter.java
+++ huahua_manager/src/main/java/com/huahua/manager/filter/ManagerFilter.java
@@ -60,6 +60,7 @@ public class ManagerFilter extends ZuulFilter {
         requestContext.setResponseBody("无权访问");
         requestContext.getResponse().setContentType("text/html;charset=UTF‐8");
         return null;
+
     }
 
 
